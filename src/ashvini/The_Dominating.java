71feb9eb8b1cd6838d1541b9b1533940607fd486
shubh <shubh@LAPTOP-1370JKBT>
2022-09-15 22:00:34 +0530

    [Ashvini]:The Mastermind

---
+++ src/ashvini/The_Dominating.java
@@ -0,0 +1,9 @@
+package ashvini;
+
+public class The_Dominating 
+{
+  public static void main(String[] args) 
+  {
+	System.out.println("much Dominating");
+  }
+}
---
+++ src/ashvini/The_Mastermind.java
@@ -0,0 +1,9 @@
+package ashvini;
+
+public class The_Mastermind 
+{
+  public static void main(String[] args) 
+  {
+	System.out.println("CHUPA RUSTOM");
+  }
+}
